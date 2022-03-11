c3111c36955995dcbe5c6f0011d8dbf40c77e47d
Andrej Bozic <abozic389@gmail.com>
2022-03-11 14:12:05 +0100

    Gameboard factory with tests.

---
+++ src/modules/gameboard.js
@@ -0,0 +1,87 @@
+import Ship from "./ship";
+
+const Gameboard = (sizeX, sizeY) => {
+  const grid = [];
+  const ships = [];
+
+  const populateBoard = () => {
+    for (let i = 0; i < sizeX; i += 1) {
+      grid.push([]);
+      for (let j = 0; j < sizeY; j += 1) {
+        grid[i].push({ type: "ocean", id: -1 });
+      }
+    }
+  };
+
+  const checkAvailableSpace = (posY, posX, shipLength, vert) => {
+    let space;
+    if (vert) {
+      space = posY + shipLength - 1 < sizeX;
+      if (space) {
+        for (let i = 0; i < shipLength; i += 1) {
+          if (grid[posY + i][posX].type === "ship") {
+            space = false;
+            break;
+          }
+        }
+      }
+    } else {
+      space = posX + shipLength - 1 < sizeY;
+      if (space) {
+        for (let i = 0; i < shipLength; i += 1) {
+          if (grid[posY][posX + i].type === "ship") {
+            space = false;
+            break;
+          }
+        }
+      }
+    }
+    return space;
+  };
+
+  const placeShip = (posY, posX, shipLength, vert) => {
+    if (!checkAvailableSpace(posY, posX, shipLength, vert)) return -1;
+    if (vert) {
+      for (let i = 0; i < shipLength; i += 1) {
+        grid[posY + i][posX] = { type: "ship", id: ships.length, shipLocation: i };
+      }
+    } else {
+      for (let i = 0; i < shipLength; i += 1) {
+        grid[posY][posX + i] = { type: "ship", id: ships.length, shipLocation: i };
+      }
+    }
+    ships.push(Ship(shipLength));
+    return 0;
+  };
+
+  const receiveAttack = (posY, posX) => {
+    if (
+      posY < 0
+      || posX < 0
+      || posY >= sizeX
+      || posX >= sizeY
+      || grid[posY][posX].hit !== undefined
+    ) {
+      return -1;
+    }
+    grid[posY][posX].hit = grid[posY][posX].type === "ship";
+    const currentPos = grid[posY][posX];
+    if (currentPos.hit) ships[currentPos.id].hit(currentPos.shipLocation);
+    return 0;
+  };
+
+  populateBoard();
+  const gameOver = () => !ships.find((ship) => !ship.isSunk());
+  const getGrid = () => grid;
+  const getShips = () => ships;
+
+  return {
+    getGrid,
+    placeShip,
+    getShips,
+    receiveAttack,
+    gameOver,
+  };
+};
+
+export default Gameboard;
---
+++ src/test/gameboard.test.js
@@ -0,0 +1,193 @@
+import Gameboard from "../modules/gameboard";
+
+describe("Gameboard factory", () => {
+  test("Gameboard with just ocean tiles", () => {
+    const gameboard = Gameboard(2, 2);
+    expect(gameboard.getGrid()).toEqual([
+      [
+        { type: "ocean", id: -1 },
+        { type: "ocean", id: -1 },
+      ],
+      [
+        { type: "ocean", id: -1 },
+        { type: "ocean", id: -1 },
+      ],
+    ]);
+  });
+
+  test("Non-square board shape", () => {
+    const gameboard = Gameboard(2, 3);
+    expect(gameboard.getGrid()).toEqual([
+      [
+        { type: "ocean", id: -1 },
+        { type: "ocean", id: -1 },
+        { type: "ocean", id: -1 },
+      ],
+      [
+        { type: "ocean", id: -1 },
+        { type: "ocean", id: -1 },
+        { type: "ocean", id: -1 },
+      ],
+    ]);
+  });
+
+  describe("Placing ships", () => {
+    test("Placing 2 tile ship - horizontal", () => {
+      const gameboard = Gameboard(2, 3);
+      gameboard.placeShip(1, 0, 2);
+      expect(gameboard.getGrid()).toEqual([
+        [
+          { type: "ocean", id: -1 },
+          { type: "ocean", id: -1 },
+          { type: "ocean", id: -1 },
+        ],
+        [
+          { type: "ship", id: 0, shipLocation: 0 },
+          { type: "ship", id: 0, shipLocation: 1 },
+          { type: "ocean", id: -1 },
+        ],
+      ]);
+    });
+
+    test("Placing 2 tile ship - vertical", () => {
+      const gameboard = Gameboard(2, 3);
+      expect(gameboard.placeShip(0, 1, 2, true)).toBe(0);
+      expect(gameboard.getGrid()).toEqual([
+        [
+          { type: "ocean", id: -1 },
+          { type: "ship", id: 0, shipLocation: 0 },
+          { type: "ocean", id: -1 },
+        ],
+        [
+          { type: "ocean", id: -1 },
+          { type: "ship", id: 0, shipLocation: 1 },
+          { type: "ocean", id: -1 },
+        ],
+      ]);
+    });
+
+    test("Placing ships without enough space", () => {
+      const gameboard = Gameboard(2, 3);
+      expect(gameboard.placeShip(1, 1, 2, true)).toBe(-1);
+      expect(gameboard.placeShip(0, 2, 2, false)).toBe(-1);
+    });
+
+    test("Cannot place ships on top of each other", () => {
+      const gameboard = Gameboard(2, 3);
+      expect(gameboard.placeShip(0, 1, 2, true)).toBe(0);
+      expect(gameboard.placeShip(0, 0, 3, false)).toBe(-1);
+      expect(gameboard.getGrid()).toEqual([
+        [
+          { type: "ocean", id: -1 },
+          { type: "ship", id: 0, shipLocation: 0 },
+          { type: "ocean", id: -1 },
+        ],
+        [
+          { type: "ocean", id: -1 },
+          { type: "ship", id: 0, shipLocation: 1 },
+          { type: "ocean", id: -1 },
+        ],
+      ]);
+    });
+
+    test("Multiple ships with different ID", () => {
+      const gameboard = Gameboard(2, 3);
+      gameboard.placeShip(0, 0, 2, false);
+      gameboard.placeShip(1, 0, 3, false);
+      expect(gameboard.getShips()[1].getLength()).toBe(3);
+      expect(gameboard.getGrid()).toEqual([
+        [
+          { type: "ship", id: 0, shipLocation: 0 },
+          { type: "ship", id: 0, shipLocation: 1 },
+          { type: "ocean", id: -1 },
+        ],
+        [
+          { type: "ship", id: 1, shipLocation: 0 },
+          { type: "ship", id: 1, shipLocation: 1 },
+          { type: "ship", id: 1, shipLocation: 2 },
+        ],
+      ]);
+    });
+  });
+
+  describe("Receiving attacks", () => {
+    test("Attack that misses", () => {
+      const gameboard = Gameboard(2, 3);
+      gameboard.receiveAttack(0, 1);
+      gameboard.receiveAttack(1, 0);
+      expect(gameboard.getGrid()).toEqual([
+        [
+          { type: "ocean", id: -1 },
+          { type: "ocean", id: -1, hit: false },
+          { type: "ocean", id: -1 },
+        ],
+        [
+          { type: "ocean", id: -1, hit: false },
+          { type: "ocean", id: -1 },
+          { type: "ocean", id: -1 },
+        ],
+      ]);
+    });
+
+    test("Attack that hits", () => {
+      const gameboard = Gameboard(2, 3);
+      gameboard.placeShip(0, 0, 2);
+      gameboard.receiveAttack(0, 1);
+      expect(gameboard.getShips()[0].getHits()).toEqual([0, 1]);
+      expect(gameboard.getGrid()).toEqual([
+        [
+          { type: "ship", id: 0, shipLocation: 0 },
+          {
+            type: "ship",
+            id: 0,
+            shipLocation: 1,
+            hit: true,
+          },
+          { type: "ocean", id: -1 },
+        ],
+        [
+          { type: "ocean", id: -1 },
+          { type: "ocean", id: -1 },
+          { type: "ocean", id: -1 },
+        ],
+      ]);
+    });
+
+    test("Invalid attack coordinates", () => {
+      const gameboard = Gameboard(2, 2);
+      expect(gameboard.receiveAttack(-1, 1)).toBe(-1);
+      expect(gameboard.receiveAttack(2, 1)).toBe(-1);
+      expect(gameboard.receiveAttack(0, -1)).toBe(-1);
+      expect(gameboard.receiveAttack(0, 2)).toBe(-1);
+    });
+
+    test("Hitting the same spot multiple times", () => {
+      const gameboard = Gameboard(2, 2);
+      expect(gameboard.receiveAttack(0, 1)).toBe(0);
+      expect(gameboard.receiveAttack(0, 1)).toBe(-1);
+      expect(gameboard.receiveAttack(0, 0)).toBe(0);
+    });
+
+    test("Sinking a ship", () => {
+      const gameboard = Gameboard(2, 2);
+      gameboard.placeShip(0, 0, 2);
+      gameboard.placeShip(1, 0, 2);
+      gameboard.receiveAttack(0, 0);
+      gameboard.receiveAttack(0, 1);
+      gameboard.receiveAttack(1, 0);
+      expect(gameboard.getShips()[0].isSunk()).toBeTruthy();
+      expect(gameboard.getShips()[1].isSunk()).toBeFalsy();
+    });
+
+    test("Sinking all of the ships", () => {
+      const gameboard = Gameboard(2, 2);
+      gameboard.placeShip(0, 0, 2);
+      gameboard.placeShip(1, 0, 2);
+      gameboard.receiveAttack(0, 0);
+      gameboard.receiveAttack(0, 1);
+      gameboard.receiveAttack(1, 0);
+      gameboard.receiveAttack(1, 1);
+      expect(gameboard.gameOver()).toBeTruthy();
+    });
+  });
+});
